62fbb452f7f06dc3806f1fe98231547e6eadc97f
Gary1235 <gary19961221@gmail.com>
2024-03-04 22:49:58 +0800

    新增 工作行程controller

---
+++ Work Journal/Areas/ScheduleManage/Controllers/WorkController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Work_Journal.Areas.ScheduleManage.Controllers
+{
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
