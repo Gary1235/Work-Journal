418fc333f36bab480cbd516071cd4d7f766fc1bb
gary.hsu <gary.hsu@hgiga.com>
2024-03-06 14:42:58 +0800

    移轉連線字串至appsettings.json

---
+++ Work Journal/ScheduleItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Work_Journal;
+
+public partial class ScheduleItem
+{
+    public Guid Id { get; set; }
+
+    public Guid ScheduleId { get; set; }
+
+    public string? WorkItem { get; set; }
+
+    public int WorkDuration { get; set; }
+}
---
+++ Work Journal/WorkJournalContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Work_Journal;
+
+public partial class WorkJournalContext : DbContext
+{
+    public WorkJournalContext()
+    {
+    }
+
+    public WorkJournalContext(DbContextOptions<WorkJournalContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Schedule> Schedules { get; set; }
+
+    public virtual DbSet<ScheduleItem> ScheduleItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DB");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Schedule>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Schedule__3214EC07B3416E9C");
+
+            entity.ToTable("Schedule");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.CreateDateTime).HasColumnType("datetime");
+            entity.Property(e => e.Subject).HasMaxLength(20);
+            entity.Property(e => e.UpdateDateTime).HasColumnType("datetime");
+            entity.Property(e => e.WorkDateTime).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<ScheduleItem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Schedule__3214EC07C29AFA91");
+
+            entity.ToTable("ScheduleItem");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.WorkItem).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
