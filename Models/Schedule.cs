8b2f3899c1d73fce0c72517501a2c9d4b154b17a
gary.hsu <gary.hsu@hgiga.com>
2024-03-06 11:58:51 +0800

    建立Model

---
+++ Models/Schedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models;
+
+public partial class Schedule
+{
+    public Guid Id { get; set; }
+
+    public string? Subject { get; set; }
+
+    public DateTime? WorkDateTime { get; set; }
+
+    public DateTime CreateDateTime { get; set; }
+
+    public DateTime? UpdateDateTime { get; set; }
+}
---
+++ Models/WorkJournalContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Models;
+
+public partial class WorkJournalContext : DbContext
+{
+    public WorkJournalContext()
+    {
+    }
+
+    public WorkJournalContext(DbContextOptions<WorkJournalContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Schedule> Schedules { get; set; }
+
+    public virtual DbSet<ScheduleItem> ScheduleItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=WorkJournal;Integrated Security=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Schedule>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Schedule__3214EC07B3416E9C");
+
+            entity.ToTable("Schedule");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.CreateDateTime).HasColumnType("datetime");
+            entity.Property(e => e.Subject).HasMaxLength(20);
+            entity.Property(e => e.UpdateDateTime).HasColumnType("datetime");
+            entity.Property(e => e.WorkDateTime).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<ScheduleItem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Schedule__3214EC07C29AFA91");
+
+            entity.ToTable("ScheduleItem");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.WorkItem).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
