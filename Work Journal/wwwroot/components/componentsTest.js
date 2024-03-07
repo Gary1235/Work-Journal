eed8916ec698be9ac13c95fadc14217a1c63c5a8
gary.hsu <gary.hsu@hgiga.com>
2024-03-07 15:49:17 +0800

    測試

---
+++ Work Journal/wwwroot/components/componentsTest.js
@@ -0,0 +1,12 @@
+﻿
+
+const templateTest = `
+    <div id="templateTest" class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">卡片資料：{{ data }}</h5>
+        </div>
+        <div>
+            <button v-on:click="AddCount()">+1</button>
+        </div>
+    </div>
+`;
